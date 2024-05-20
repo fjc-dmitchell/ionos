9349ec0e189ccf874dd6c69eac07a6d10e023860
Doug Mitchell <dmitchell@fjc.gov>
2024-05-20 18:25:20 -0400

    multisite configuration, htaccess, gitignore

---
+++ web/sites/sites.php
@@ -0,0 +1,7 @@
+<?php
+
+$sites['dougsdarkroom.com'] = 'darkroom';
+$sites['rayconniff.info'] = 'rc';
+$sites['percyfaith.info'] = 'pf';
+$sites['bertkaempfert.info'] = 'bk';
+$sites['ferranteandteicher.info'] = 'ft';
